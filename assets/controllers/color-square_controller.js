1513eb1feb5ba0cd238e75ee16749212d9363eeb
skh M1 <simonhillskh@gmail.com>
2024-11-05 19:17:50 +1100

    finished lesson 9.

---
+++ assets/controllers/color-square_controller.js
@@ -0,0 +1,20 @@
+import {Controller} from '@hotwired/stimulus';
+
+export default class extends Controller {
+  
+  static targets = ['colorSquare', 'select'];
+  
+  connect() {
+    this.selectTarget.classList.add('d-none');
+  }
+  
+  selectColor(event) {
+    
+    this.colorSquareTargets.forEach((element) => {
+      element.classList.remove('selected');
+    });
+    
+    event.currentTarget.classList.add('selected');
+    this.selectTarget.value = event.currentTarget.dataset.colorId;
+  }
+}
