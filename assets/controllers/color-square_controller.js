9c733af893f9138f8415b029f250b70b2158f445
skh M1 <simonhillskh@gmail.com>
2024-11-07 18:52:05 +1100

    finished lesson 11.

--- assets/controllers/color-square_controller.js
+++ assets/controllers/color-square_controller.js
@@ -3,18 +3,39 @@ import {Controller} from '@hotwired/stimulus';
 export default class extends Controller {
   
   static targets = ['colorSquare', 'select'];
+  selectedColorId = '';
   
   connect() {
     this.selectTarget.classList.add('d-none');
   }
   
   selectColor(event) {
+    this.setSelectedColor(event.currentTarget.dataset.colorId);
+  }
+  
+  setSelectedColor(clickedColorId) {
+    if (clickedColorId === this.selectedColorId) {
+      this.findSelectedColorSquare().classList.remove('selected');
+      this.selectedColorId = '';
+      // this.selectTarget = '';
+      return;
+    }
+    
+    this.selectedColorId = clickedColorId;
     
     this.colorSquareTargets.forEach((element) => {
       element.classList.remove('selected');
     });
     
-    event.currentTarget.classList.add('selected');
-    this.selectTarget.value = event.currentTarget.dataset.colorId;
+    this.findSelectedColorSquare().classList.add('selected');
+    this.selectTarget.value = this.selectedColorId;
   }
+  
+  /**
+   * @returns {Element|null}
+   */
+  findSelectedColorSquare() {
+    return this.colorSquareTargets.find((element) => element.dataset.colorId === this.selectedColorId);
+  }
+  
 }
