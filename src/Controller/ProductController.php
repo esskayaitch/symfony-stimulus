0ad52014e8983d1b78bd79d5b5dffa738b01a28e
skh M1 <simonhillskh@gmail.com>
2024-11-10 15:18:56 +1100

    fixed lesson 16.

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -1,37 +1,47 @@
 <?php
 
-namespace App\Controller;
+  namespace App\Controller;
 
-use App\Entity\Category;
-use App\Entity\Product;
-use App\Form\AddItemToCartFormType;
-use App\Repository\CategoryRepository;
-use App\Repository\ProductRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
+  use App\Entity\Category;
+  use App\Entity\Product;
+  use App\Form\AddItemToCartFormType;
+  use App\Repository\CategoryRepository;
+  use App\Repository\ProductRepository;
+  use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+  use Symfony\Component\HttpFoundation\Request;
+  use Symfony\Component\Routing\Annotation\Route;
+  use Symfony\Component\HttpFoundation\Response;
 
-class ProductController extends AbstractController
-{
+  class ProductController extends AbstractController
+  {
     /**
      * @Route("/", name="app_homepage")
      * @Route("/category/{id}", name="app_category")
      */
-    public function index(Request $request, CategoryRepository $categoryRepository, ProductRepository $productRepository, Category $category = null): Response
-    {
-        $searchTerm = $request->query->get('q');
-        $products = $productRepository->search(
-            $category,
-            $searchTerm
-        );
+    public function index(
+      Request $request,
+      CategoryRepository $categoryRepository,
+      ProductRepository $productRepository,
+      Category $category = null
+    ): Response {
+      $searchTerm = $request->query->get('q');
+      $products = $productRepository->search(
+        $category,
+        $searchTerm
+      );
 
-        return $this->render('product/index.html.twig', [
-            'currentCategory' => $category,
-            'categories' => $categoryRepository->findAll(),
-            'products' => $products,
-            'searchTerm' => $searchTerm
+      if ($request->query->get('preview')) {
+        return $this->render('product/_searchPreview.html.twig', [
+          'products' => $products,
         ]);
+      }
+
+      return $this->render('product/index.html.twig', [
+        'currentCategory' => $category,
+        'categories'      => $categoryRepository->findAll(),
+        'products'        => $products,
+        'searchTerm'      => $searchTerm
+      ]);
     }
 
     /**
@@ -39,15 +49,15 @@ class ProductController extends AbstractController
      */
     public function showProduct(Product $product, CategoryRepository $categoryRepository): Response
     {
-        $addToCartForm = $this->createForm(AddItemToCartFormType::class, null, [
-            'product' => $product
-        ]);
+      $addToCartForm = $this->createForm(AddItemToCartFormType::class, null, [
+        'product' => $product
+      ]);
 
-        return $this->render('product/show.html.twig', [
-            'product' => $product,
-            'currentCategory' => $product->getCategory(),
-            'categories' => $categoryRepository->findAll(),
-            'addToCartForm' => $addToCartForm->createView()
-        ]);
+      return $this->render('product/show.html.twig', [
+        'product'         => $product,
+        'currentCategory' => $product->getCategory(),
+        'categories'      => $categoryRepository->findAll(),
+        'addToCartForm'   => $addToCartForm->createView()
+      ]);
     }
-}
+  }
