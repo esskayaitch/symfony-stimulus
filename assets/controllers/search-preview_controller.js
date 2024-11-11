674c989c082ee3bd097422dfb7961f35828a8dcb
skh M1 <simonhillskh@gmail.com>
2024-11-11 11:30:42 +1100

    fixed lesson 17.

--- assets/controllers/search-preview_controller.js
+++ assets/controllers/search-preview_controller.js
@@ -1,18 +1,33 @@
-import {Controller} from '@hotwired/stimulus'
+import {Controller} from '@hotwired/stimulus';
+import {useClickOutside, useDebounce} from 'stimulus-use';
 
 export default class extends Controller {
   static values = {
     url: String
   }
+  static targets = ['result', 'input'];
+  static debounces = ['search'];
   
-  static targets = ['result']
+  connect() {
+    useClickOutside(this);
+    useDebounce(this, {wait: 400});
+  }
   
-  async onSearchInput(event) {
+  onSearchInput(event) {
+    this.search();
+  }
+  
+  async search() {
     const params = new URLSearchParams({
-      q: event.currentTarget.value,
+      q: this.inputTarget.value,
       preview: 1
     });
     const response = await fetch(`${this.urlValue}?${params.toString()}`);
+    
     this.resultTarget.innerHTML = await response.text();
+  }
+  
+  clickOutside(event) {
+    this.resultTarget.innerHTML = '';
   }
 }
